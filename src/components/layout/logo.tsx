748a14b8bdb344440568aed2a55dc1f9b50095af
deepak rankawat <deepakrankawatcode@gmail.com>
2025-12-22 09:02:45 +0000

    w-45 h 45 logo

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 export function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("flex items-center gap-3", className)}>
-      <div className="relative w-48 h-48">
+      <div className="relative w-43 h-43">
         <Image 
           src="/images/logo.png" 
           alt="Monumenta Projects Logo" 
